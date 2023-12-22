1cb06851afbebd6b2db1b453cab323ba6d4f327a
jhs512 <jangka512@gmail.com>
2023-12-22 19:00:39 +0900

    front

---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let names = $state([]);
+
+	$effect(() => {
+		fetch('http://localhost:8090/api/v1/home/names')
+			.then((res) => res.json())
+			.then((data) => {
+				names = data;
+			});
+	});
+</script>
+
+<h1>이름들</h1>
+
+<ul>
+	{#each names as name}
+		<li>{name}</li>
+	{/each}
+</ul>
