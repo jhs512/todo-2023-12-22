d162fffa5c4521da0ed90bdca888a25ddaeae5d9
jhs512 <jangka512@gmail.com>
2023-12-24 00:05:40 +0900

    work

--- src/main/java/com/ll/todo20231222/global/initData/All.java
+++ src/main/java/com/ll/todo20231222/global/initData/All.java
@@ -6,23 +6,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 
-@Profile("!prod")
 @Configuration
 @RequiredArgsConstructor
-public class NotProd {
+public class All {
     private final MemberService memberService;
 
     @Bean
-    @Order(4)
-    ApplicationRunner initNotProd() {
+    @Order(2)
+    ApplicationRunner initAll() {
         return args -> {
-            if (memberService.findByUsername("user4").isPresent()) return;
+            if (memberService.findByUsername("admin").isPresent()) return;
 
-            Member memberUser3 = memberService.join("user3", "1234").getData();
-            Member memberUser4 = memberService.join("user4", "1234").getData();
+            Member memberAdmin = memberService.join("admin", "1234").getData();
+            Member memberUser1 = memberService.join("user1", "1234").getData();
+            Member memberUser2 = memberService.join("user2", "1234").getData();
         };
     }
 }
--- src/main/java/com/ll/todo20231222/global/initData/Dev.java
+++ src/main/java/com/ll/todo20231222/global/initData/Dev.java
@@ -6,12 +6,14 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 
 @Profile("dev")
 @Configuration
 @RequiredArgsConstructor
 public class Dev {
     @Bean
+    @Order(3)
     ApplicationRunner initDev() {
         return args -> {
             String cmd = "npx openapi-typescript http://localhost:8090/v3/api-docs/API%20V1 -o ./front/src/lib/types/api/v1/schema.d.ts";
