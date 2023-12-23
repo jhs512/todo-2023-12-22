535b776aa6a87df1264807d338d2dda09b6e4fd5
jhs512 <jangka512@gmail.com>
2023-12-23 15:38:32 +0900

    work

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	let names = $state([]);
+    let names = $state([]);
 
-	$effect(() => {
-		fetch('http://localhost:8090/api/v1/home/names')
-			.then((res) => res.json())
-			.then((data) => {
-				names = data;
-			});
-	});
+    $effect(() => {
+        fetch('http://localhost:8090/api/v1/home/names')
+            .then((res) => res.json())
+            .then((data) => {
+                names = data;
+            });
+    });
 </script>
 
 <h1>이름들</h1>
 
 <ul>
-	{#each names as name}
-		<li>{name}</li>
-	{/each}
+    {#each names as name}
+        <li>{name}</li>
+    {/each}
 </ul>
---
+++ src/main/java/com/ll/todo20231222/domain/home/home/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.ll.todo20231222.domain.home.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public List<String> getNames() {
+        return List.of("Tom", "Paul", "John");
+    }
+}
